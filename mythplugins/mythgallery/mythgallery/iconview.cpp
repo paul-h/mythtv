d9ea14c050034980ef55307cb86d391bb868c01b
Paul Harrison <mythtv@sky.com>
2012-11-14 22:57:31 +0000

    mythgallery: Restore position after a delete in icon view
    Move back to the same position after deleting and image. It was annoying to
    have the selected item move to the first item after a delete especially when
    deleting several images.

--- mythplugins/mythgallery/mythgallery/iconview.cpp
+++ mythplugins/mythgallery/mythgallery/iconview.cpp
@@ -1041,6 +1041,11 @@ void IconView::DoDeleteCurrent(bool doDelete)
     {
         ThumbItem *thumbitem = GetCurrentThumb();
 
+        int currPos = 0;
+        MythUIButtonListItem *item = m_imageList->GetItemCurrent();
+        if (item)
+            currPos = m_imageList->GetCurrentPos();
+
         if (!thumbitem)
             return;
 
@@ -1049,6 +1054,8 @@ void IconView::DoDeleteCurrent(bool doDelete)
         GalleryUtil::Delete(fi);
 
         LoadDirectory(m_currDir);
+
+        m_imageList->SetItemCurrent(currPos);
     }
 }
 
