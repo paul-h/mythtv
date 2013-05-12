3219e9996202039dc67a4bae825114a1decc2f6c
Paul Harrison <mythtv@sky.com>
2013-05-12 23:23:53 +0100

    Add 'Music' and 'MusicArt' to the list of special storage groups

--- mythtv/libs/libmythbase/storagegroup.cpp
+++ mythtv/libs/libmythbase/storagegroup.cpp
@@ -30,6 +30,8 @@ const QStringList StorageGroup::kSpecialGroups = QStringList()
     << "Fanart"
     << "Screenshots"
     << "Banners"
+    << "Music"
+    << "MusicArt"
     ;
 
 /****************************************************************************/
